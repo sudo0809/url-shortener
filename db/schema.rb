750cccb9fca7f0140ec52680c751a0556cfc134c
sugat dhole <sugatdhole@gmail.com>
2024-01-04 18:13:49 +0530

    postgresql setup

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2024_01_02_055505) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "short_urls", force: :cascade do |t|
     t.text "redirect_url"
     t.string "short_url"
